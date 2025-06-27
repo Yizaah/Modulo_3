aec447fca400fb1e3b5c725f918fa0299d1fcb12
Yitzhak Valenzuela <yitzhak.1995.valenzuela@gmail.com>
2025-06-26 20:38:18 -0400

    20:38

---
+++ Semana 1/26/variables_numericas.py
@@ -0,0 +1,25 @@
+import math
+
+#Ejemplo 1: Calcular el área de unt triángulo
+
+base = 6.6
+altura = 8.2
+
+area=(base * altura)/2
+area= round(area)
+print(area)
+
+
+distancia_norte= 30
+distancia_este= 40
+
+distancia_directa= math.hypot(distancia_norte,distancia_este)
+print("la distancia recorrida por el árbitro es:", distancia_directa)
+
+
+#math.ceil para redondear hacia arriba. math.floor para redondear hacia abajo. trunk corta los decimales al que uno le dice. 
+
+velocidad = 6.3 # MB/s
+tiempo = 60 #segundos
+mb_descargados = math.floor(velocidad * tiempo)
+print (f"descargaste {mb_descargados} MB en {tiempo} segundos ({velocidad*tiempo}).")
